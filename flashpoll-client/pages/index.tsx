8efafc3c225f2c67fe6740ad54a7003f3f058a38
AnshVM <anshmalik2002@gmail.com>
2023-04-01 02:54:04 +0530

    remove unwanted css

--- flashpoll-client/pages/index.tsx
+++ flashpoll-client/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
 
 export default function Home() {
   return (
@@ -10,8 +9,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/flashpoll.ico" />
       </Head>
-      <main className={styles.main}>
-        <h1 className='text-3xl font-bold'>Welcome to Flashpoll ⚡</h1>
+      <main>
+        <h1 className='text-3xl font-bold text-center mt-10'>Welcome to Flashpoll ⚡</h1>
       </main>
     </>
   )
