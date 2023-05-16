0ebd27ca18ba270579416f6069c9725b5fd01e7b
AnshVM <anshmalik2002@gmail.com>
2023-05-17 04:52:55 +0530

    update api url

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path',
-        destination: 'http://localhost:8080/:path*'
+        destination: 'https://flashpoll.onrender.com/:path*'
       }
     ]
   }
